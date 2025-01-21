b47b350bbfc8326fb7501c9aeb625a62d52d0488
smitb852 <bagadia.s@flipkart.com>
2025-01-21 17:31:39 +0530

    business package added

--- JEDI-GROUP-H-JAVA-FLIPFIT-POS/src/com/flipkart/business/CustomerOperations.java
+++ JEDI-GROUP-H-JAVA-FLIPFIT-POS/src/com/flipkart/business/CustomerOperations.java
@@ -10,16 +10,28 @@ public class CustomerOperations {
 	public void createCustomer(){
 		System.out.println("This method is used to create a customer");
 	}
+	
 	public boolean updateCustomer(int id){
-		System.out.println("Customer is deleted by id");
+		System.out.println("Customer is updated by id" + id);
 		return true;
 	}
 	public boolean deleteCustomer(int id) {
-		System.out.println("Customer is deleted by id");
+		System.out.println("Customer is deleted by id" + id);
 		return true;
 	}
-	public void listCustomer() {
-		System.out.println("");
+//	public void listCustomer() {
+//		System.out.println("");
+//	}
+	
+	public void requestBooking(Slot slot, Gym gym, String userId) {
+		System.out.println("Customer requests booking\n");
 	}
 	
+	public void cancelBooking(String bookingId, String userId) {
+		System.out.println("Customer cancels booking\n");
+	}
+	
+	public void viewBooking(String userId) {
+		System.out.println("Bookings: \n");
+	}
 }
