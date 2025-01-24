400a49dde541d4b42e52867306ed398152f8231a
SHRISHTI003 <shrishti.akhouri@flipkart.com>
2025-01-24 09:49:51 +0530

    Add dao

--- JEDI-GROUP-H-JAVA-FLIPFIT-POS/src/com/flipkart/dao/AdminDaoImpl.java
+++ JEDI-GROUP-H-JAVA-FLIPFIT-POS/src/com/flipkart/dao/AdminDaoImpl.java
@@ -1,5 +1,159 @@
 package com.flipkart.dao;
 
-public class AdminDaoImpl {
+import  com.flipkart.jdbc.DemoJdbc;
+import  com.flipkart.bean.GymOwner;
+import  com.flipkart.bean.GymCentre;
+import  com.flipkart.constants.SQLConstants;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AdminDaoImpl {
+	
+	@Override
+	public void approveGym(String gymId) {
+		try {
+			con=DemoJdbc.connect();
+			System.out.println("Fetching gym centres");
+			
+			String sqlQuery = "UPDATE GymCenter SET status = "approved" WHERE gym_id = ?";
+			statement=con.prepareStatement(sqlQuery);
+			statement.setString(1,gymId);
+			statement.executeUpdate();
+			
+			List<GymCentre> approvedGyms = viewGyms(); 
+	        for (GymCentre gym : approvedGyms) {
+	            System.out.println("Gym ID: " + gym.getGymId() + ", Name: " + gym.getName() + ", Location: " + gym.getLocation());
+	        }
+
+        } catch (SQLException se) {
+            se.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+			
+		}
+	
+	@Override
+	public void  approveGymOwner(String userId) {
+	 try {
+		 con=DemoJdbc.connect();
+         System.out.println("Fetching gyms owners");
+
+         String sqlQuery = "UPDATE User SET status = "approved" WHERE gym_id = ?";
+         statement = con.prepareStatement(sqlQuery);
+         statement.setString(1, ownerId);
+         statement.executeUpdate();
+         
+
+     } catch (SQLException se) {
+         se.printStackTrace();
+     } catch (Exception e) {
+         e.printStackTrace();
+     }
+	}
+	
+	@Overrideg
+	public List<GymCentre> viewGyms()
+	{
+		 try {
+			    con=DemoJdbc.connect();
+	            System.out.println("Fetching gyms centres");
+	            
+	            String sqlQuery = "SELECT * FROM GymCenters WHERE status="approved"";
+	            statement = con.prepareStatement(sqlQuery);
+	            ResultSet rs = statement.executeQuery();
+
+	            List<GymCentre> gymCentres = new ArrayList<>();
+	            while(rs.next()) {
+	                Gym gymCentre = new GymCentre(
+	                        rs.getString("gymId"),
+	                        rs.getString("name"),
+	                        rs.getString("location"),
+	                        rs.getString("gymOwnerId"),
+	                );
+	                gymCentres.add(gymCentre);
+	            }
+	            return gymCentres;
+	        }
+	        catch (SQLException se)
+	        {
+	            se.printStackTrace();
+	            return null;
+	        }
+	        catch (Exception e)
+	        {
+	            e.printStackTrace();
+	            return null;
+	        }
+	}
+	
+	@Override
+	public List<GymOwner> viewPendingGymOwners() {
+		try {
+		    con=DemoJdbc.connect();
+            System.out.println("Fetching pending gymOwner approval");
+            
+            String sqlQuery = "SELECT * FROM User WHERE status = "pending"";
+            statement = con.prepareStatement(sqlQuery);
+            ResultSet rs = statement.executeQuery();
+
+            List<GymOwner> gymOwners  = new ArrayList<>();
+            while(rs.next()) {
+                GymOwner gymOwner = new GymOwner(
+                        rs.getString("name"),
+                        rs.getString("email"),
+                );
+                gymOwners.add(gymOwner);
+            }
+            return gymOwners;
+        }
+        catch (SQLException se)
+        {
+            se.printStackTrace();
+            return null;
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+            return null;
+        }
+	}
+	
+	@Override
+	public List<GymCentre>  viewPendingGyms() {
+		try {
+		    con=DemoJdbc.connect();
+            System.out.println("Fetching pending gym approval");
+            
+            String sqlQuery = "SELECT * FROM GymCenter WHERE status = "pending"";
+            statement = con.prepareStatement(sqlQuery);
+            ResultSet rs = statement.executeQuery();
+
+            List<GymCentre> gymCentres  = new ArrayList<>();
+            while(rs.next()) {
+                GymCentre gymCentre = new GymCentre(
+                	    rs.getString("gymId"),
+                        rs.getString("name"),
+                        rs.getString("location"),
+                        rs.getString("gymOwnerId"),);
+                gymCentres.add(gymCentre);
+            }
+            return gymCentres;
+        }
+        catch (SQLException se)
+        {
+            se.printStackTrace();
+            return null;
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+            return null;
+        }
+	}
 }
--- JEDI-GROUP-H-JAVA-FLIPFIT-POS/src/com/flipkart/dao/PaymentDaoImpl.java
+++ JEDI-GROUP-H-JAVA-FLIPFIT-POS/src/com/flipkart/dao/PaymentDaoImpl.java
@@ -1,5 +1,66 @@
 package com.flipkart.dao;
 
-public class PaymentDaoImpl {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
+public class PaymentDao {
+
+    // Method to make a payment from sender to receiver and return the payment ID
+    public int makePayment(int receiverId, int senderId, int amount) {
+        Connection con = null;
+        PreparedStatement statement = null;
+        ResultSet rs = null;
+
+        try {
+            // Establish the database connection
+            con = DemoJdbc.connect();
+
+            // 1. Get the current max payment_id
+            String maxPaymentIdQuery = "SELECT MAX(payment_id) AS max_id FROM Payments";
+            statement = con.prepareStatement(maxPaymentIdQuery);
+            rs = statement.executeQuery();
+
+            int newPaymentId = 1;  // Default to 1 if no payments exist yet
+
+            // If there are records, we can increment the max payment_id
+            if (rs.next()) {
+                newPaymentId = rs.getInt("max_id") + 1;
+            }
+
+            // 2. Insert the new payment record with the generated payment_id
+            String insertPaymentQuery = "INSERT INTO Payments (payment_id, sender_id, receiver_id, amount, status) VALUES (?, ?, ?, ?, 'pending')";
+            statement = con.prepareStatement(insertPaymentQuery);
+            statement.setInt(1, newPaymentId); // Set the generated payment_id
+            statement.setInt(2, senderId);
+            statement.setInt(3, receiverId);
+            statement.setInt(4, amount);
+
+            // Execute the insert statement
+            int rowsAffected = statement.executeUpdate();
+
+            // 3. Return the generated payment_id if insertion was successful
+            if (rowsAffected > 0) {
+                return newPaymentId;
+            }
+
+            return -1; // In case of failure
+
+        } catch (SQLException se) {
+            se.printStackTrace();
+            return -1; // Return -1 if there's an error
+        } catch (Exception e) {
+            e.printStackTrace();
+            return -1; // Return -1 if there's an error
+        } finally {
+            try {
+                if (rs != null) rs.close();
+                if (statement != null) statement.close();
+                if (con != null) con.close();
+            } catch (SQLException se) {
+                se.printStackTrace();
+            }
+        }
+    }
 }
