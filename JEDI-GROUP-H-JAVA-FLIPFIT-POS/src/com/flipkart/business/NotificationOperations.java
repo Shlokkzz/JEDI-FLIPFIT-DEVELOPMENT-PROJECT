c4427f2b8a96824c7eb392479f2fb150dc9322a3
DarshD5 <darsh.bhavindoshi@flipkart.com>
2025-01-24 14:04:37 +0530

    fix business 1

--- JEDI-GROUP-H-JAVA-FLIPFIT-POS/src/com/flipkart/business/NotificationOperations.java
+++
@@ -1,8 +0,0 @@
-package com.flipkart.business;
-
-public class NotificationOperations {
-	
-	public void notifyUser(String userId) {
-		System.out.println("User: " + userId + " is notified\n");
-	}
-}
--- JEDI-GROUP-H-JAVA-FLIPFIT-POS/src/com/flipkart/business/PaymentOperations.java
+++
@@ -1,9 +0,0 @@
-package com.flipkart.business;
-
-public class PaymentOperations {
-	
-	public void makePayment(int receiverId, int senderId, int amount) {
-		
-		System.out.println("Payment received\n");
-	}
-}
